b47ea1d07495ac7462c8d53d8a6fb7d5a9b1f6a7
Wellington Leocádio <108426770+Wleocadio@users.noreply.github.com>
2023-04-06 17:38:53 -0300

    correção arquivo yml

--- features/support/env.rb
+++ features/support/env.rb
@@ -13,7 +13,7 @@ World(Pages, Helper)
 
 
 Capybara.configure do |config|
-    config.default_driver = :selenium_chrome
+    config.default_driver = :selenium_chrome_headless
     config.app_host = CONFIG['url_default']
     config.default_max_wait_time = 10
 
