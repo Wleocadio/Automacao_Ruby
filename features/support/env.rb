acfc3a56ca3452f47afaf20e1eb9fdb19c37c482
Wellington Leocádio <108426770+Wleocadio@users.noreply.github.com>
2023-04-06 17:09:54 -0300

    correção arquivo yml

--- features/support/env.rb
+++ features/support/env.rb
@@ -13,18 +13,9 @@ World(Pages, Helper)
 
 
 Capybara.configure do |config|
-    config.default_driver = :selenium_chrome
+    config.default_driver = :selenium_chrome_headless
     config.app_host = CONFIG['url_default']
     config.default_max_wait_time = 10
 
 end
 
-Capybara.register_driver :selenium_chrome_headless do |app|
-    options = Selenium::WebDriver::Chrome::Options.new(args: %w[headless disable-gpu])
-  
-    Capybara::Selenium::Driver.new(
-      app,
-      browser: :chrome,
-      options: options
-    )
-  end
