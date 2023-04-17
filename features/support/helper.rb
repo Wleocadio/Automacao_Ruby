f3ec659ac834e553fde4ca6ff65f92a06098c143
Wellington Leocádio <108426770+Wleocadio@users.noreply.github.com>
2023-04-17 10:03:23 -0300

    Atualizando arquivos

--- features/support/helper.rb
+++ features/support/helper.rb
@@ -2,7 +2,7 @@ module Helper
 
     def print_screen(file_name, result)
     date = "#{Time.now.strftime("%Y/%B/%D")}"
-    hour = "#{Time.now.strftime("%H-%M-%S")}"
+    hour = "#{Time.now.strftime("%H-%M")}"
 
     file_path = "reports/screenshots/teste_#{result}"
     screenshots = "#{file_path}/#{date}/#{hour}/#{file_name}.png"
